52ffa598d929d2ccad185779ea86602993858f72
Rahi-007 <linnima072@gmail.com>
2026-02-28 23:33:15 +0600

    style updated

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 // <!--W_---------------{  Create  Date : 28/02/2026  }---------------_-->
 // <!--W_---------------{  Last Update  : 28/02/2026  }---------------_-->
+
 const container = document.getElementById("days");
+const currentDay = 2;
 
-const currentDay = 1;
-
-for (let i = 1; i <= 10; i++) {
-  const day = String(i).padStart(2, "0");
-
-  const div = document.createElement("div");
-
+for (let i = 1; i <= 28; i++) {
   const isDisabled = i > currentDay;
+  const day = String(i).padStart(2, "0");
+  const div = document.createElement("div");
 
   div.innerHTML = `
       <a href="${isDisabled ? "#" : `./src/day_${day}/index.html`}" 
          class="
-           font-semibold flex items-center gap-2
+           flex items-center justify-center gap-2
+           p-4 rounded-xl border
+           font-semibold text-sm
+           transition-all duration-300
            ${
              isDisabled
-               ? "opacity-40 cursor-not-allowed pointer-events-none text-gray-400"
-               : "hover:text-blue-600"
+               ? "opacity-40 cursor-not-allowed pointer-events-none text-gray-400 bg-gray-100"
+               : "hover:text-sky-600 hover:bg-sky-50 hover:shadow-md"
            }
          ">
         <i class="fa-solid fa-folder"></i>
