cf938e95f301428e1704a0ab4d069f7eccaad391
Rahi-007 <linnima072@gmail.com>
2026-03-03 22:23:59 +0600

    2nd question added done

---
+++ index.ts
@@ -0,0 +1,7 @@
+// <!--W_---------------{  Create  Date : 03/03/2026  }---------------_-->
+// <!--W_---------------{  Last Update  : 03/03/2026  }---------------_-->
+
+function longestCommonPrefix(strs: string[]): string {
+  let common: string = "";
+  return common;
+}
