02ecbae5fffb9ad45bcfc8fa7e9c8d8476238169
Rahi-007 <rubaiedahammed007@gmail.com>
2026-09-19 17:14:51 +0600

    add a console app

---
+++ Console_App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = 12;
+        Program program = new Program();
+        int result = program.Reverse(num);
+        Console.WriteLine($"Reverse {num} = {result}");
+    }
+    public int Reverse(int x)
+    {
+        return 0;
+    }
+}
