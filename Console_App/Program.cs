0050d480300bb01aead99e4c037958c997fc27ee
Rahi-007 <rubaiedahammed007@gmail.com>
2026-09-20 22:51:10 +0600

    day 08 solved

--- Console_App/Program.cs
+++ Console_App/Program.cs
@@ -4,13 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        int num = 12;
+        int num = 1236;
         Program program = new Program();
         int result = program.Reverse(num);
         Console.WriteLine($"Reverse {num} = {result}");
     }
     public int Reverse(int x)
     {
-        return 0;
+        int reverseNum = 0;
+        while (x != 0)
+        {
+            int lastDigit = x % 10;
+            x /= 10;
+
+            if (reverseNum > int.MaxValue / 10 ||
+                (reverseNum == int.MaxValue / 10 && lastDigit > 7))
+            {
+                return 0;
+            }
+
+            if (reverseNum < int.MinValue / 10 ||
+                (reverseNum == int.MinValue / 10 && lastDigit < -8))
+            {
+                return 0;
+            }
+
+            reverseNum = reverseNum * 10 + lastDigit;
+        }
+
+        return reverseNum;
     }
 }
